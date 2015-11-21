1b38c7bff84af415fd3e1d3ee2eef2c6d2ef6f1e
Viacheslav Makarov <makarov.viacheslav@gmail.com>
2015-11-22 00:56:07 +0300

    Added javadoc artifacts as their absence violates OSSRH Requirements.

--- error-handling/src/main/java/me/mekarthedev/tools/error/ErrorHandling.java
+++ error-handling/src/main/java/me/mekarthedev/tools/error/ErrorHandling.java
@@ -8,6 +8,17 @@ public class ErrorHandling {
         void call() throws Exception;
     }
 
+    /**
+     * Wraps the given call in assertion that it will not throw.
+     * <p>
+     * Implements typical try-catch-rethrow technique for dealing with checked exceptions
+     * in situations where they are expected not to be thrown. The given callable is called and the result is returned.
+     * If the action throws, the exception is rethrown as a RuntimeException.
+     *
+     * @param action The callable to be called to get the result.
+     * @param <Result> Type of the resulting value.
+     * @return Result of action.call().
+     */
     public static <Result> Result noThrow(Callable<Result> action) {
         try {
             return action.call();
